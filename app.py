9235448b66c76191bfc2d436cc0ce9992fc2c43c
matheusgrilo <matheusgrilo@live.com>
2025-01-24 14:03:20 -0300

    Add endpoint to delete a product by ID
    Introduced a DELETE /api/products/delete/<id> endpoint to remove a product. It checks if the product exists and returns appropriate success or error messages. Enhances API functionality for managing products.

--- app.py
+++ app.py
@@ -26,5 +26,15 @@ def add_product():
     else:
         return jsonify({'message': 'Missing name or price in the request data'}), 400
 
+@app.route('/api/products/delete/<int:product_id>', methods=['DELETE'])
+def delete_product(product_id):
+    product = db.session.get(Product, product_id) or ()
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({'message': 'Product deleted successfully!'})
+    else:
+        return jsonify({'message': 'Product not found'}), 404
+
 if __name__ == "__main__":
     app.run(debug=True)
